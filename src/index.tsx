5c1d7c9f48083f8fb7325066fffba645cb9c01a0
darcee <darcee.thomason@gmail.com>
2025-08-15 11:00:32 -0700

    Adds AntD

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { ConfigProvider, theme, App as AntApp } from 'antd';
+import 'antd/dist/reset.css';
 import './index.css'
 import App from './App.tsx'
 import { Amplify } from "aws-amplify";
@@ -7,6 +9,17 @@ import outputs from "../amplify_outputs.json";
 Amplify.configure(outputs);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <ConfigProvider
+          theme={{
+              algorithm: theme.defaultAlgorithm, // switch to theme.darkAlgorithm for dark
+              token: {
+                  colorPrimary: '#1677ff', // customize your brand color
+              },
+          }}
+      >
+          <AntApp>
+            <App />
+          </AntApp>
+      </ConfigProvider>
   </StrictMode>,
 )
